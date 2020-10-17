194cbdfac880d036d6727f6dee78a3173adc6f00
alex89620109009 <alex89620109009@gmail.com>
2020-10-17 05:15:44 +0300

    Добавьте файлы проекта.

---
+++ Швейная Фабрика/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Швейная_Фабрика
+{
+    partial class FormAutoriz
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.LoginTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.LogButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(64, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Авторизация";
+            // 
+            // LoginTextBox
+            // 
+            this.LoginTextBox.Location = new System.Drawing.Point(56, 154);
+            this.LoginTextBox.Multiline = true;
+            this.LoginTextBox.Name = "LoginTextBox";
+            this.LoginTextBox.Size = new System.Drawing.Size(197, 22);
+            this.LoginTextBox.TabIndex = 1;
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Location = new System.Drawing.Point(56, 219);
+            this.PasswordTextBox.Multiline = true;
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.Size = new System.Drawing.Size(197, 22);
+            this.PasswordTextBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(56, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Логин";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(56, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Пароль";
+            // 
+            // LogButton
+            // 
+            this.LogButton.BackColor = System.Drawing.Color.Transparent;
+            this.LogButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.LogButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LogButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LogButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.LogButton.Location = new System.Drawing.Point(109, 294);
+            this.LogButton.Name = "LogButton";
+            this.LogButton.Size = new System.Drawing.Size(89, 39);
+            this.LogButton.TabIndex = 5;
+            this.LogButton.Text = "Войти";
+            this.LogButton.UseVisualStyleBackColor = false;
+            this.LogButton.Click += new System.EventHandler(this.LogButton_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(39, 256);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(225, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Не верный логин или пароль";
+            this.label4.Visible = false;
+            // 
+            // FormAutoriz
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
+            this.ClientSize = new System.Drawing.Size(311, 363);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.LogButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.LoginTextBox);
+            this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "FormAutoriz";
+            this.Text = "Швейная фабрика";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox LoginTextBox;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button LogButton;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Швейная Фабрика/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Швейная_Фабрика
+{
+   
+    public partial class FormAutoriz : Form
+    {
+        
+        const string con = @"Data Source=ПК-ПК\SQLEXPRESS;Initial Catalog=Fabrika;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(con);
+        
+        public FormAutoriz()
+        {
+            InitializeComponent();
+            PasswordTextBox.PasswordChar = '*';
+            
+        }
+
+        private void LogButton_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+             
+            DataTable table = new DataTable();
+            String loginUser = LoginTextBox.Text;
+            String passUser = PasswordTextBox.Text;
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                SqlCommand command = new SqlCommand("SELECT * FROM Пользователь WHERE Логин = @uL  AND Пароль = @pU ", connection);
+            command.Parameters.Add("@uL", SqlDbType.VarChar).Value = loginUser;
+            command.Parameters.Add("@pU", SqlDbType.VarChar).Value = passUser;
+            adapter.SelectCommand = command;
+                adapter.Fill(table);
+
+                if (table.Rows.Count > 0)
+                {
+                label4.Visible = false;
+
+                MessageBox.Show("Da");
+                }
+                else
+                {
+                label4.Visible = true;
+                }
+
+            connection.Close();
+            
+        }
+
+      
+    }
+}
